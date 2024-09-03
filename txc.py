c8bc63aac2f9c2469d583be8301c22263051a1f0
itxaShubhamChauhan <aniketchauhan2k@gmail.com>
2024-09-03 14:03:07 +0530

    Update txc.py

--- txc.py
+++ txc.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = 'TOKEN'
-ADMIN_ID = 'YOUR ID'
+API_TOKEN = '7234351984:AAHZNfNBUulc3Nd5CsQA5Xu5a3W2ELUFYEk'
+ADMIN_ID = '729917819'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
